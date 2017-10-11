f203b2647e1b17bbd81153c8a824d79a42c734ae
Uyer <martin.teysera@gmail.com>
2017-10-11 16:59:29 +0200

    5.3 Added Mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+  default from: "from@example.com"
+  def contact_form(email, name, message)
+    @message = message
+    mail(:from => email,
+    :to => 'your-email@example.com',
+    :subject => "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
+  @appname = "Spintailz"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  post 'static_pages/thank_you'
+
   get 'static_pages/index'
   root 'static_pages#landing_page'
 
