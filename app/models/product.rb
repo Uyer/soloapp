accbfcf481d9ed559034e36c2c460e12d3258853
Uyer <martin.teysera@gmail.com>
2017-10-23 12:45:07 +0200

    5.7 Added Comment Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,23 @@
 class Product < ApplicationRecord
 has_many :orders
+has_many :comments, dependent: :destroy
 
-def self.search(search_term)
-  if Rails.env.production?
-      Product.where("name ilike ?", "%#{search_term}%")
-  else
-      Product.where("name LIKE ?", "%#{search_term}%")
+  def self.search(search_term)
+    if Rails.env.production?
+        Product.where("name ilike ?", "%#{search_term}%")
+    else
+        Product.where("name LIKE ?", "%#{search_term}%")
+
+    end
 
   end
 
+  def highest_rating_comment
+    comments.rating_desc.first
   end
+  
+  def lowest_rating_comment
+		comments.rating_asc.first
+	end
 
 end
