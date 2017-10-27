c018dcfb3082ab29d24678ac0e47105bcb7d56cd
Uyer <martin.teysera@gmail.com>
2017-10-27 21:32:25 +0200

    5.9 Added Validation and Pagination

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :manage, :all
     can :read, :all
     # Define abilities for the passed in user here. For example:
     #
