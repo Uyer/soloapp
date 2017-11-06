7682d80d38990b9ec93f59e33f653ad44f075105
Uyer <martin.teysera@gmail.com>
2017-11-06 19:42:25 +0100

    6.3 Functional Test-2

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,32 +1,35 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe UsersController, type: :controller do
-  @user = { User.create!(email: 'totoron@gmail.com', password: 'testpassword')}
-  @user2 = { User.create!(email: 'tongo@boboo.com', password: 'testpassword2')}
-    
-   describe "GET #show" do
-     context 'when a user is logged in' do
-        before do
-          sign_in @user
-        end
+	let (:user) {User.create!(email: "spec@test.com", password: "passwordtest")}
+	let (:user_2) {User.create!(email: "spec_user2@test.com", password: "passwordtest2")}
 
-       it 'loads correct user details' do
-          get :show, params: { id: @user.id }
-          expect(response).to be_ok
-          expect(assigns(:user)).to eq @user
-        end
+	describe "GET #show" do
 
-       it 'redirects to root path' do
-          get :show, params: { id: @user2.id }
-          expect(response).to redirect_to(root_path)
-        end
-     end
-      
-     context 'when a user is not logged in' do
-        it 'redirects to login' do
-          get :show, params: { id: @user.id }
-          expect(response).to redirect_to(root_path)
-        end
-      end
-    end
-  end
+		context "when user is logged in" do
+			before do
+				sign_in user
+			end
+
+			it "loads correct user details" do
+				get :show, params: {id: user.id}
+				expect(response).to be_ok
+				expect(assigns(:user)).to eq user
+			end
+
+			it "restricts access to other users" do
+				get :show, params: {id: user_2.id}
+				expect(response).to have_http_status(302)
+				expect(response).to redirect_to(root_path)
+			end
+		end
+
+		context "when user is not logged in" do
+			it "redirects to login" do
+				get :show, params: {id: user.id}
+				expect(response).to redirect_to(new_user_session_path)
+			end
+		end
+
+	end
+end
