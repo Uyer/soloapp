0b5a63cad3804dcef366a3339290be17d6e85d16
Uyer <martin.teysera@gmail.com>
2017-11-13 16:07:30 +0100

    6.6 Creating Payments

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
 
   post 'static_pages/thank_you'
+  post 'payments/create'
+  get 'payments/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
