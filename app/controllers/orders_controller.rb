374caaab07033cc32d58e7723579a2b2157b33fc
Uyer <martin.teysera@gmail.com>
2017-11-20 20:19:44 +0100

    Changed order_controller User sees only his orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
+    @user = current_user
+    if user_signed_in? && @user.admin?
     @orders = Order.all
+    elsif user_signed_in?
+      @orders = @user.orders
+    else
+      redirect_to main_app.root_url, alert: "You are not logged in"
+    end
   end
 
   def show
